c91bc3aa293f15c2ce9d44ad284fb2e44d35b324
Yasushi Ikeda <yikeda6616@gmail.com>
2019-05-17 12:01:59 -0700

    Update module index

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,3 +1,19 @@
 import { combineReducers } from 'redux';
+import * as Todos from './todos';
+import * as VisibilityFilter from './visibilityFilter';
 
-export const rootReducer = combineReducers({});
+// RootStateとrootReducerのキー名が一致していないといけない点に注意
+export type RootState = {
+  todos: Todos.State;
+  visibilityFilter: VisibilityFilter.State;
+};
+
+export const rootReducer = combineReducers({
+  todos: Todos.reducer,
+  visibilityFilter: VisibilityFilter.reducer
+});
+
+export const actionCreator = {
+  todos: Todos.actionCreator,
+  visibilityFilter: VisibilityFilter.actionCreator
+};
