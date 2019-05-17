ed14b71783c9f80f6ab81cfa64af2b0910e852dd
Yasushi Ikeda <yikeda6616@gmail.com>
2019-05-17 12:46:04 -0700

    Add Todo component

---
+++ src/components/Todo.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+type Props = {
+  text: string;
+  completed: boolean;
+  onClick: () => void;
+};
+
+const Todo: React.FC<Props> = (props: Props) => {
+  return (
+    <li
+      onClick={props.onClick}
+      style={{ textDecoration: props.completed ? 'line-through' : 'none' }}
+    >
+      {props.text}
+    </li>
+  );
+};
+
+export default Todo;
