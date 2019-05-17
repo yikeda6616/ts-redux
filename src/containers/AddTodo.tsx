3b30e497386e12b132c58c90bbcc9879fc29470f
Yasushi Ikeda <yikeda6616@gmail.com>
2019-05-17 13:46:59 -0700

    Add AddTodo container

---
+++ src/containers/AddTodo.tsx
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux';
+import { Action, Dispatch } from 'redux';
+
+import { actionCreator } from '../modules';
+import AddTodo from '../components/AddTodo';
+
+const mapStateToProps = () => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch: Dispatch<Action>) => {
+  return {
+    onSubmit: (text: string) => {
+      dispatch(actionCreator.todos.addTodo({ text }));
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AddTodo);
